3de5b8bb29c5a7f4a7a7442a2a99bd05c16daf2d
M-Aman Siddiqui <m.amansiddiqui2024@gmail.com>
2025-01-15 00:35:49 -0800

    completed

--- src/MobileAppDevelopment/Component/BgAppHero.jsx
+++ src/MobileAppDevelopment/Component/BgAppHero.jsx
@@ -3,30 +3,29 @@ import bgappHero from '../../assets/Images/BgAppDevelopment.mp4';
 
 function BgAppHero() {
   return (
-    <div>
+    <div className="relative bg-black text-white">
+      {/* Heading */}
+      <div className="inset-0 z-10 flex justify-center">
+        <h1 className="text-4xl md:text-3xl lg:text-5xl items-center font-bold pt-10 font-serif underline">
+          MOBILE APP DEVELOPMENT
+        </h1>
+      </div>
 
-          <div className="bg-black w-full py-16 text-white">
-      
-      <h1 className="text-4xl md:text-4xl text-center font-bold font-serif mt-12 underline">
-        MOBILE APP DEVELOPMENT
-      </h1>
-</div> 
-    <div className="relative w-full h-screen   overflow-hidden z-0 ">
-   
-      <video
-        className="absolute top-0 left-0 w-full h-screenx  object-cover z-0" 
-        autoPlay
-        loop
-        muted
-        playsInline
-      >
-        <source src={bgappHero} type="video/mp4" />
-      </video>
-    </div></div>
+      {/* Background Video */}
+      <div className="w-full h-screen overflow-hidden">
+        <video
+          className="mt-10 left-0 w-full h-full object-cover"
+          autoPlay
+          loop
+          muted
+          playsInline
+        >
+          <source src={bgappHero} type="video/mp4" />
+          Your browser does not support the video tag.
+        </video>
+      </div>
+    </div>
   );
 }
 
 export default BgAppHero;
-
-
-
